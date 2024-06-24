61da2337b2a2237c40fcea2dc25cadc3facf477b
Dieter <dieter.vanderelst@fastmail.net>
2024-06-24 11:11:14 -0400

    clean settings

--- settings.py
+++ settings.py
@@ -1,7 +1,5 @@
 import pyb
 
-# Common settings
-
 red = pyb.LED(1)
 green = pyb.LED(2)
 blue = pyb.LED(3)
@@ -12,19 +10,6 @@ adc_pin2 = 'X8'
 trigger_pin2 = 'X2'
 
 data_sep = ','
-
-# For field context
-
-servo_pin = 'X6'
-servo_pulse_range = [500, 2500] # in usecs
-
-signal_threshold = 2000
-sample_rate = 20000
-duration = 25
-repeats = 3
-
-
-# For robot context
 
 wifi_ssid = 'batnet'
 wifi_password = 'lebowski'
